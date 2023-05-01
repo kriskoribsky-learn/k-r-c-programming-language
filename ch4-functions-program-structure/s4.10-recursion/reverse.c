2c1491acfd942826d08085944395cc0c363935fc
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-05-01 23:01:34 +0200

    exercise 4-13

---
+++ ch4-functions-program-structure/s4.10-recursion/reverse.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char *s);
+
+int main(void)
+{
+    char s[256] = "hello world";
+
+    printf("%s\n", s);
+    reverse(s);
+    printf("%s\n", s);
+}
+
+void reverse(char *s)
+{
+    static int i = 0;
+    static int j = 0;
+
+    if (s[i] != '\0')
+    {
+        char c = s[i++];
+        reverse(s);
+
+        s[j++] = c;
+    }
+}
