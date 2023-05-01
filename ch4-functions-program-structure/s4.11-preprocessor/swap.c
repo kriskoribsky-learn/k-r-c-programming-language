d8c144acaf50a59b76f6243859e7d57ef340a6be
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-05-01 23:09:20 +0200

    exercise 4-14

---
+++ ch4-functions-program-structure/s4.11-preprocessor/swap.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define swap(t, x, y) \
+    {                 \
+        t tmp = x;    \
+        x = y;        \
+        y = x;        \
+    }
+
+int main(void)
+{
+    int x = 2, y = 3;
+
+    printf("x: %d, y: %d\n", x, y);
+
+    swap(int, x, y);
+    printf("x: %d, y: %d\n", x, y);
+
+    return 0;
+}
+
+// NOTE: A use of a block is very useful because there can be created local
+// variables that don't create conflicts with already existed entities.
