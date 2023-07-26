55a51f6a03ffdcd480f9b73a0480cb4ca0f022cb
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-07-26 23:10:26 +0200

    feat: exercise 5-1

--- ch5-pointers-and-arrays/s5.3-pointers-and-arrays/getint.c
+++ ch5-pointers-and-arrays/s5.3-pointers-and-arrays/getint.c
@@ -1,8 +1,60 @@
+#include <stdio.h>
+#include <stdbool.h>
 #include <ctype.h>
+
+#include "getch.c"
 
 int getch(void);
 void ungetch(int);
+int getint(int *pn);
+
+int main(void)
+{
+    int num;
+
+    printf("Enter a number:\n");
+    if (!getint(&num))
+    {
+        printf("Error! Not a number.\n");
+        return 1;
+    }
+
+    printf("Converted number is: '%d'\n", num);
+}
 
 int getint(int *pn)
 {
+    int c, sign;
+
+    // skip whitespace
+    while (isspace(c = getch()) != 0)
+        ;
+
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-')
+    {
+        ungetch(c);
+        return 0;
+    }
+
+    sign = (c == '-') ? -1 : 1;
+
+    if ((c == '-' || c == '+') && !isdigit(c = getch()))
+    {
+        ungetch(c);
+        ungetch(sign == -1 ? '-' : '+');
+        return 0;
+    }
+
+    for (*pn = 0; isdigit(c); c = getch())
+    {
+        *pn = *pn * 10 + (c - '0');
+    }
+
+    *pn *= sign;
+
+    if (c != EOF)
+    {
+        ungetch(c);
+    }
+    return c;
 }
