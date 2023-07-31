a204de44c2e2e6b04483b0a3fba74a9660b67717
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-07-31 23:55:36 +0200

    feat: begin exercise 5-4

---
+++ ch5-pointers-and-arrays/s5.6-pointer-arrays-pointers-to-pointers/strend.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_LEN 100
+
+bool strend(char *restrict s, const char *restrict end);
+
+int main(void)
+{
+    char s1[2 * MAX_LEN];
+    char s2[MAX_LEN];
+
+    printf("First string: ");
+    scanf("%s", s1);
+
+    printf("Second string: ");
+    scanf("%s", s2);
+
+    printf("First string ends with second string: '%s'\n", strend(s1, s2) ? "True" : "False");
+}
+
+bool strend(char *restrict s, const char *restrict end)
+{
+    while (*s++ || *end++)
+        ;
+
+    return *end == '\0' && *s ;
+}
