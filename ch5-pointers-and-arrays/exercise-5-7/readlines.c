4685faf66d29dc5e32c83008b9fec4cc78dbb86a
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-08-14 22:16:54 +0200

    feat: exercise 5-7

--- ch5-pointers-and-arrays/exercise-5-7/readlines.c
+++ ch5-pointers-and-arrays/exercise-5-7/readlines.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAXLINES 5000 /* max lines to be sorted */
+#define MAXLEN 100    /* max length of any input line */
 
-char *lineptr[MAXLINES]; /* pointers to input lines */
-
-int readlines(char *lineptr[], int nlines);
+int readlines(char *lineptr[], char *linebuf, int maxlines);
 void writelines(char *lineptr[], int nlines);
 
-void qsort(char *lineptr[], int left, int right);
+void my_qsort(char *lineptr[], int left, int right);
+
+int my_getline(char *s, int lim);
 
 /* sort input lines */
 int main(void)
 {
     int nlines;
+    char linebuf[MAXLINES * MAXLEN]; /* storage of input lines */
+    char *lineptr[MAXLINES];         /* pointers to input lines */
 
-    if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
+    if ((nlines = readlines(lineptr, linebuf, MAXLINES)) >= 0)
     {
+        my_qsort(lineptr, 0, nlines - 1);
+        printf("--------------------\n");
         writelines(lineptr, nlines);
         return 0;
     }
@@ -27,25 +33,23 @@ int main(void)
     }
 }
 
-#define MAXLEN 100 /* max length of any input line */
-int getline(char *, int);
-
-int readlines(char *lineptr[], int maxlines) /* read input lines */
+int readlines(char *lineptr[], char *linebuf, int maxlines) /* read input lines */
 {
     int len, nlines;
-    char *p, line[MAXLEN];
+    char buffer[MAXLEN];
 
     nlines = 0;
-    while ((len = getline(line, MAXLEN)) > 0)
+    while ((len = my_getline(buffer, MAXLEN)) > 0)
     {
-        if (nlines >= maxlines || (p = malloc(len * sizeof(char))) == NULL)
+        if (nlines >= maxlines)
         {
             return -1;
         }
 
-        line[len - 1] = '\0'; /* delete newline */
-        strcpy(p, line);
-        lineptr[nlines++] = p;
+        buffer[len - 1] = '\0'; /* delete '\n' */
+        strcpy(linebuf, buffer);
+        lineptr[nlines++] = linebuf;
+        linebuf += len; /* shift buffer space */
     }
     return nlines;
 }
@@ -56,4 +60,57 @@ void writelines(char *lineptr[], int nlines) /* write ouput lines */
     {
         printf("%s\n", *lineptr++);
     }
+}
+
+void my_qsort(char *v[], int left, int right) /* sort v[left]...v[right] into increasing order */
+{
+    int i, last;
+    void swap(char *v[], int i, int j);
+
+    if (left >= right)
+    {
+        return;
+    }
+
+    swap(v, left, left + (right - left) / 2);
+    last = left;
+
+    for (i = left + 1; i <= right; i++)
+    {
+        if (strcmp(v[i], v[left]) < 0)
+        {
+            swap(v, ++last, i);
+        }
+    }
+
+    swap(v, left, last);
+
+    my_qsort(v, left, last - 1);
+    my_qsort(v, last + 1, right);
+}
+
+void swap(char *v[], int i, int j) /* interchange v[i] and v[j] */
+{
+    char *temp;
+
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+int my_getline(char *s, int lim) /* read a line into s, return length */
+{
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+    {
+        s[i] = c;
+    }
+    if (c == '\n')
+    {
+        s[i++] = c;
+    }
+
+    s[i] = '\0';
+    return i;
 }
