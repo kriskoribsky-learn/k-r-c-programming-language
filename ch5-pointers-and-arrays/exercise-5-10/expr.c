d2786d87c390503bfdd95ce4fc016049111e3ccf
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-09-02 23:59:23 +0200

    feat: add stack layout

--- ch5-pointers-and-arrays/exercise-5-10/expr.c
+++ ch5-pointers-and-arrays/exercise-5-10/expr.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAXVAL 100 /* maximum depth of val stack */
+
+int sp = 0;         /* next free stack position */
+double val[MAXVAL]; /* value stack */
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
