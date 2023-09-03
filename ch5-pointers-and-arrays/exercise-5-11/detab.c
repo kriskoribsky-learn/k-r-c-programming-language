ae4717de1afa2e1efc7059da6e937231b02645d0
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-09-04 00:48:15 +0200

    feat: finish exercise 5-11

--- ch5-pointers-and-arrays/exercise-5-11/detab.c
+++ ch5-pointers-and-arrays/exercise-5-11/detab.c
@@ -3,11 +3,25 @@
 
 #define DEFAULT_TAB_SIZE 8 /* Number of spaces the tab '\t' character should represent */
 
+/* this routine will take only tab size as optional argument instead of list of tab stops */
 int main(int argc, char **argv)
 {
+    int tab_size = argc == 2 ? atoi(argv[1]) : DEFAULT_TAB_SIZE; /* in case of argument error tabs will be deleted */
 
-
-
-
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+        {
+            for (int i = 0; i < tab_size; i++)
+            {
+                putchar(' ');
+            }
+        }
+        else
+        {
+            putchar(c);
+        }
+    }
     return EXIT_SUCCESS;
 }
---
+++ ch5-pointers-and-arrays/exercise-5-11/entab.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEFAULT_TAB_SIZE 8 /* Number of spaces the tab '\t' character should represent */
+
+/* this routine will take only tab size as optional argument instead of list of tab stops */
+int main(int argc, char **argv)
+{
+    int tab_size = argc == 2 ? atoi(argv[1]) : DEFAULT_TAB_SIZE; /* in case of argument error tabs will be deleted */
+    int current_blank = 0;
+
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+        {
+            current_blank++;
+        }
+        else
+        {
+            int tabs = current_blank / tab_size;
+            int spaces = current_blank % tab_size;
+
+            for (int i = 0; i < tabs; i++)
+            {
+                putchar('\t');
+            }
+            for (int i = 0; i < spaces; i++)
+            {
+                putchar(' ');
+            }
+            current_blank = 0;
+
+            putchar(c); /* put back the last non-blank character */
+        }
+    }
+    return EXIT_SUCCESS;
+}
