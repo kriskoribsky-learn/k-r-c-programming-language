1498b68257a6ce78150ad99fde8b290f0cfd4a36
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-09-03 23:56:47 +0200

    feat: setup detab routine structure

---
+++ ch5-pointers-and-arrays/exercise-5-11/detab.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEFAULT_TAB_SIZE 8 /* Number of spaces the tab '\t' character should represent */
+
+int main(int argc, char **argv)
+{
+
+
+
+
+    return EXIT_SUCCESS;
+}
