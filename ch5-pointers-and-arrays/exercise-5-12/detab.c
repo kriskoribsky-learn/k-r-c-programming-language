e5b35aa78ef9342d3f44bbfecbd990a72deac8d7
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-09-08 00:33:06 +0200

    feat: command-line argument handling for detab

--- ch5-pointers-and-arrays/exercise-5-12/detab.c
+++ ch5-pointers-and-arrays/exercise-5-12/detab.c
@@ -1,28 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DEFAULT_TAB_SIZE 8 /* Number of spaces the tab '\t' character should represent */
-#define DEFAULT_TAB_STOP 0 /* Number of spaces to skip before starting replacing '\t' */
+#define DEFAULT_TAB_SIZE 8  /* Number of spaces the tab '\t' character should represent */
+#define DEFAULT_TAB_START 0 /* Number of spaces to skip before starting replacing '\t' */
 
 /* this routine will take only tab size as optional argument instead of list of tab stops */
 int main(int argc, char **argv)
 {
-    int tab_size = argc == 2 ? atoi(argv[1]) : DEFAULT_TAB_SIZE; /* in case of argument error tabs will be deleted */
+    long tab_size = DEFAULT_TAB_SIZE;
+    long tab_start = DEFAULT_TAB_START;
 
-    int c;
-    while ((c = getchar()) != EOF)
+    /* read command-line arguments */
+    long val;
+    char *valptr;
+    char *endptr;
+
+    while (--argc)
     {
-        if (c == '\t')
+        switch ((*++argv)[0])
         {
-            for (int i = 0; i < tab_size; i++)
+        case '-': /* tab size */
+            valptr = *argv + 1;
+            val = strtol(valptr, &endptr, 10);
+
+            if (endptr == valptr) /* there were no digits at all */
             {
-                putchar(' ');
+                fprintf(stderr, "Error: please provide a numberic argument value for tab stop\n");
+                exit(EXIT_FAILURE);
             }
-        }
-        else
-        {
-            putchar(c);
+
+            tab_start = val;
+            break;
+
+        case '+': /* tab start */
+            valptr = *argv + 1;
+            val = strtol(valptr, &endptr, 10);
+
+            if (endptr == valptr) /* there were no digits at all */
+            {
+                fprintf(stderr, "Error: please provide a numberic argument value for tab size\n");
+                exit(EXIT_FAILURE);
+            }
+
+            tab_size = val;
+            break;
         }
     }
+
+    // int c;
+    // // while ((c = getchar()) != EOF)
+    // // {
+    // //     if (c == '\t')
+    // //     {
+    // //         for (int i = 0; i < tab_size; i++)
+    // //         {
+    // //             putchar(' ');
+    // //         }
+    // //     }
+    // //     else
+    // //     {
+    // //         putchar(c);
+    // //     }
+    // // }
+
     return EXIT_SUCCESS;
 }
