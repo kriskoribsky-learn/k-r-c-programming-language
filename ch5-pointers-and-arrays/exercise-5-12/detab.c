ae82f37578a7943b1433abee37976eb647e4f533
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-09-07 23:55:11 +0200

    feat: init exercise 5-12 detab

---
+++ ch5-pointers-and-arrays/exercise-5-12/detab.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEFAULT_TAB_SIZE 8 /* Number of spaces the tab '\t' character should represent */
+#define DEFAULT_TAB_STOP 0 /* Number of spaces to skip before starting replacing '\t' */
+
+/* this routine will take only tab size as optional argument instead of list of tab stops */
+int main(int argc, char **argv)
+{
+    int tab_size = argc == 2 ? atoi(argv[1]) : DEFAULT_TAB_SIZE; /* in case of argument error tabs will be deleted */
+
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+        {
+            for (int i = 0; i < tab_size; i++)
+            {
+                putchar(' ');
+            }
+        }
+        else
+        {
+            putchar(c);
+        }
+    }
+    return EXIT_SUCCESS;
+}
