cd5fe53814466c4c4711da1ac865a8e3de2594e6
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-09-12 00:07:56 +0200

    feat: exercise 5-12 init entab structure

---
+++ ch5-pointers-and-arrays/exercise-5-12/entab.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEFAULT_TAB_SIZE 8  /* Number of spaces the tab '\t' character should represent */
+#define DEFAULT_TAB_START 0 /* Number of spaces to skip before starting replacing '\t' */
+
+int main(int argc, char **argv)
+{
+    long tab_size = DEFAULT_TAB_SIZE;
+    long tab_start = DEFAULT_TAB_START;
+
+    /* read command-line arguments */
+    long val;
+    char *valptr;
+    char *endptr;
+
+    while (--argc)
+    {
+        switch ((*++argv)[0])
+        {
+        case '-': /* tab size */
+            valptr = *argv + 1;
+            val = strtol(valptr, &endptr, 10);
+
+            if (endptr == valptr) /* there were no digits at all */
+            {
+                fprintf(stderr, "Error: please provide a numberic argument value for tab stop\n");
+                exit(EXIT_FAILURE);
+            }
+
+            tab_start = val;
+            break;
+
+        case '+': /* tab start */
+            valptr = *argv + 1;
+            val = strtol(valptr, &endptr, 10);
+
+            if (endptr == valptr) /* there were no digits at all */
+            {
+                fprintf(stderr, "Error: please provide a numberic argument value for tab size\n");
+                exit(EXIT_FAILURE);
+            }
+
+            tab_size = val;
+            break;
+        }
+    }
+    return EXIT_SUCCESS;
+}
